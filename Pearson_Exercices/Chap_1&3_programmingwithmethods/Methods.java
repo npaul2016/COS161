a892ae572527c7d779032459ddd547e2486bdd94
Jean Paul Ngiruwera <npaul2016@users.noreply.github.com>
2018-10-18 09:24:38 -0400

    Assignments
    test

---
+++ Pearson_Exercices/Chap_1&3_programmingwithmethods/Methods.java
@@ -0,0 +1,22 @@
+
+public class FancyHello {
+
+    public static void main(String[] args) {
+        hello();
+        System.out.println();
+        hello();
+        System.out.println();
+        hello();
+
+
+    }
+
+    public static void hello() {
+        System.out.println("///************\\\\\\");
+        System.out.println("||| hello world! |||");
+        System.out.println("\\\\\\************///");
+
+
+    }
+
+}
