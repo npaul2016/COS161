47cb1c1bfb41fe5592e1c18f91911ca548acf361
Jean Paul Ngiruwera <npaul2016@users.noreply.github.com>
2018-10-19 20:36:40 -0400

    Methods > Drawing with methods
    Drawing shapes with Methods.
    Project name in Eclipse: StaticMethod

---
+++ Pearson_Exercices/Chap_1&3_programmingwithmethods/eggfigure.java
@@ -0,0 +1,35 @@
+public class EggFigures {
+
+    public static void main(String[] args) {
+
+        System.out.println("  ------");
+        System.out.println(" /      \\");
+        System.out.println("/        \\");
+
+        System.out.println("\\        /");
+        System.out.println(" \\      /");
+        System.out.println("  ------");
+
+        System.out.println(" \\      /");
+        System.out.println("  \\    /");
+        System.out.println("+-------+");
+
+        System.out.println("  ------");
+        System.out.println(" /      \\");
+        System.out.println("/        \\");
+        System.out.println("|  STOP  |");
+        System.out.println("\\        /");
+        System.out.println(" \\      /");
+        System.out.println("  ------");
+
+        System.out.println("  ------");
+        System.out.println(" /      \\");
+        System.out.println("/        \\");
+        System.out.println("+--------+");
+
+
+
+
+    }
+
+}
