8e02b3c6a1b3b955dd1aa785879513279d6c4f2a
Jean Paul Ngiruwera <npaul2016@users.noreply.github.com>
2018-10-19 23:36:20 -0400

    Complete code
    Drawing complex figures with stati methods

--- Pearson_Exercices/Chap_1&3_programmingwithmethods/eggfigure.java
+++ Pearson_Exercices/Chap_1&3_programmingwithmethods/eggfigure.java
@@ -1,35 +1,65 @@
 public class EggFigures {
 
     public static void main(String[] args) {
+        egg();
+        cup();
+        stop();
+        hat();
+    }
+    
+        public static void egg() {
+           eggTop();
+           eggBottom();
+           System.out.println();
+        }
 
-        System.out.println("  ------");
-        System.out.println(" /      \\");
-        System.out.println("/        \\");
-        System.out.println();
-        System.out.println("\\        /");
-        System.out.println(" \\      /");
-        System.out.println("  ------");
-        System.out.println();
-        System.out.println(" \\      /");
-        System.out.println("  \\    /");
-        System.out.println("+-------+");
-        System.out.println();
-        System.out.println("  ------");
-        System.out.println(" /      \\");
-        System.out.println("/        \\");
+        public static void eggTop() {
+            System.out.println("  ------");
+            System.out.println(" /      \\");
+            System.out.println("/        \\");
+        }
+
+        public static void eggBottom() {
+            System.out.println("\\        /");
+            System.out.println(" \\      /");
+            System.out.println("  ------");
+        }
+
+        public static void line() {
+            System.out.println("+-------+");
+        }
+
+        public static void cup() {
+
+            eggBottom();
+            line();
+            System.out.println();
+        }
+
+        public static void stop() {
+
+          eggTop();
         System.out.println("|  STOP  |");
-        System.out.println("\\        /");
-        System.out.println(" \\      /");
-        System.out.println("  ------");
-        System.out.println();
-        System.out.println("  ------");
-        System.out.println(" /      \\");
-        System.out.println("/        \\");
-        System.out.println("+--------+");
+          eggBottom();
+          System.out.println();
+        }
+
+        public static void hat() {
 
 
+        eggTop();
+          line();
+
+        }
 
 
     }
 
-}
+
+
+
+/* Development strategy:
+1)Unstructured
+2)Structured
+3)Structured without redundancy
+*/
