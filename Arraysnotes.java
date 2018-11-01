70119cf57a59a717836150eee4ee80b39d719ff7
Jean Paul Ngiruwera <npaul2016@users.noreply.github.com>
2018-11-01 13:01:30 -0400

    Arrayslist 11/1/2018

---
+++ Arraysnotes.java
@@ -0,0 +1,17 @@
+/*
+Arrays and Arrays lists
+Java collection framework:
+List :a collection storing an ordered sequence of elements
+
+Idea of a list:
+
+Rather than creating an array of boxes, create an object that represent a "list" of items.(Initially an empty list)
+
+you can add items to the list.
+[hello, ABC, goodbye, okay]
+
+
+ArrayList methods (10.1)
+
+
+*/
---
+++ Classnotes/ArrayList.java
@@ -0,0 +1,74 @@
+//11/1/2018 ( ArraysList)
+
+//using remove methods in an array list
+//
+System.out.println("Remove all occ. of Pam:" + strList.removeAll(Arrays.asList("Pam")));
+System.out.println(strList);
+
+//trimToSize();
+//Three dimension array list
+// Learning about classes : look up Java Api Specification: is a huge web page containing documentation
+// every java class and its methods.
+
+// ArrayList vs. Array 2
+
+
+ArrayList<String> allWords = new ArrayList<string>();
+Scanner input = new Scanner  (new File ("words.txt"))
+while (input.hasNext)()) {
+  String word = input.next ();
+  allWords.add(word);
+}
+
+System.out.println(allWords);
+
+// remove all plurar words
+
+for (int i = 0; i < allWords.size(); i++ ) {
+}
+
+
+// ArrayList as paramater
+
+public  static void name (ArrayList <type> name ) {
+
+  Example:
+
+  // removes all plura word frm the give list.
+
+
+}
+
+
+// ArrayList of primitives ?
+
+//The type yu specify
+
+
+//Wrapper classes (2Pac)
+
+// Primitve Types
+
+Excercice:
+
+//
+
+
+//ArrayList mystery
+
+ArrayList<Integer> list = new ArrayList<Integer>();
+for (int i = 1; i <= 10; i++) {
+
+  list.add(2*1); // [2, 4, 6, 8, 10]
+}
+
+
+
+what is the output of the following code ?
+
+int size = list.size();
+for (int i = 0; i < size; i++) {
+  list.add(i, 42); // add 42 at index i
+}
+
+System.out.println(list);
