615292626c0530df4a31e3f55b133a8b2b3d900c
Jean Paul Ngiruwera <npaul2016@users.noreply.github.com>
2018-11-06 12:54:02 -0500

    Arraylist cont

---
+++ Classnotes/Arraylist11062018.java
@@ -0,0 +1,53 @@
+// write a method hat add a star
+
+public static void addStars(ArrayList<String> list) {
+  for (int )
+}
+
+// Object storing collections
+// The compareTo method (10.2) :
+//example:
+public int compareTo (String other)
+
+String a = "alice";
+String b = "bob";
+if (a.compareTo(b)) <0) { //true
+  ...
+
+}
+
+// compareTo and collections > binary search
+// You can use an array or list of strings with Javan's included binary search method
+// because it calls compareTo internally.
+
+String[] a = {"al" , "bob", "cari", "dan", "mike" }
+
+
+int index = Arrays.binarySearch(a, "dan"); // 3
+
+//Ordering our own types :
+
+//comparable template
+
+public class name implements Comparable<name> {
+
+}
+
+//Comparable example
+public class Point implements Comparable<point> {
+  private int x;
+  private int y;
+  ..
+}
+
+// sort by x and break ties by y
+
+public int compareTo (Point other) {
+  if (x !=other.x) {
+    return x - other.x; // different x
+
+  }
+}
+
+
+// Exercice > Make the HtmlTags 
---
+++ Classnotes/Sets&Maps11062018.java
@@ -0,0 +1,89 @@
+// Java collections frameworks : check out what that means
+// programs that counts the number of unique words in a large text file ( say, moby dick or the king james bible)
+// store the words in a collections and report the # of unique ids
+
+// Empirical analysis
+
+System.currentTimeMillis()
+
+//Sets: a collections of unit values, or set of elements ( no duplicate allowed)
+// they don't have indexes, you want to know if the value of data exist or not with the stet
+
+// Set implementation
+
+Set Interface //in
+java.util
+
+//Set is implemented by */ HashSet and TreeSet
+
+//  Set methods
+
+list<String> list = new ArrayList<String> ();
+..
+set<Integer> set = new TreeSet<Integer>();
+Set<String> set2 = new HashSet<String>(list);
+
+- can construct an empty set, or one based on a given collection.
+
+//Set Operations
+
+addAll, retainAll, removeAll
+
+
+//Sets and Ordering
+
+//HashSet: elements are stored in an unpredictable order
+
+import java.util.*;
+
+public final class SetMapExample {
+
+
+  public static void public static void main(String[] args) {
+
+  }
+
+  Set<String> names = new HashSet<String>();
+
+  names.add("Jakes");
+  names.add("Robert");
+  names.add("Marisa");
+  names.add("Kasey");
+
+  System.out.println("names");
+}
+
+Set<String> names2 = new TreeSet<String>(names);
+System.out.println(names2);
+
+Set<String> names3= new HashSet<String>(names);
+System.out.println(names3);
+
+
+// [ Kasey, Robert, Jakes, Marisa]
+
+
+//The "for each " loop ( 7.10)
+
+for ( type name : collection) {
+statements;
+}
+
+//Maps: keys then functions ( maps are like )
+
+put( key, value ): adds a mapping from a key to a value
+get(key); retrieves the value mapped to the key.
+remove(key): removes the give key and its mapped value
+
+// maps and tallying
+// the index key doesn't have to be an int
+
+// recall prevous tallying examples from CSE 143
+/// count digits: 22092310907
+
+// Map implementation
+
+Map interface in java.util
+implemented by HashMap and TreeMap classes
+
+Maps methods (list )
