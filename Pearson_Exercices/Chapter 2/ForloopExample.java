59250f301974977d513e6358f89066b4741383c9
Jean Paul Ngiruwera <npaul2016@users.noreply.github.com>
2018-11-11 23:48:19 -0500

    For Loops

---
+++ Pearson_Exercices/Chapter 2/ForloopExample.java
@@ -0,0 +1,11 @@
+public class LoopExample {
+   public static void main(String[] args) {
+
+     for (int i=1; i<=3; i++){
+       System.out.println(i + " Squared = " + (i*i));
+     }
+     System.out.println("Godbye");
+   }
+
+}
+  
